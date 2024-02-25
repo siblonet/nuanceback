6a19099cf973f82619399f3ddd5303a2f4510086
John Kokar <princedibloni4@gmail.com>
2024-02-25 10:58:26 +0000

    doinee

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ActivityService } from './home.service';
 import { ActivityController } from './home.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AnnonceSchema, ArticleSchema } from './dto/create-activity.dto';
+import { AnnonceSchema, ArticleSchema, VersionAvailabeSchema } from './dto/create-activity.dto';
 import { MineindService } from 'src/mineind/mineind.service';
 import { OrderSchema } from 'src/order/dto/create-order.dto';
 
@@ -10,6 +10,7 @@ import { OrderSchema } from 'src/order/dto/create-order.dto';
   imports: [
     MongooseModule.forFeature([
       { name: 'NuanceDoud', schema: ArticleSchema },
+      { name: 'VersionAvailabe', schema: VersionAvailabeSchema },
       { name: 'OrderDoud', schema: OrderSchema },
       { name: 'AnnonceDoud', schema: AnnonceSchema }
     ])
